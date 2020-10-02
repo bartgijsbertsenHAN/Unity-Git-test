7a3d089ee9244949ba3f024048b58ce6f1df263d
Bart Gijsbertsen <GLJ.Gijsbertsen@student.han.nl>
2020-10-02 15:04:50 +0200

    Added rotation to the player (Q+E)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
 
     public float movementSpeed;
+    public float rotationSpeed;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        float cameraRotation = Input.GetAxis("CameraRotation") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, cameraRotation, 0, Space.Self);
     }
 
     void FixedUpdate()
