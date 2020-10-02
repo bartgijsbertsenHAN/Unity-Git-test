7a528860a14a515b80e3062522743da1daf5b453
Bart Gijsbertsen <GLJ.Gijsbertsen@student.han.nl>
2020-10-02 14:48:57 +0200

    Camera follows player
    And flipped horizontal movement input

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform playerTransform;
+
+    void Update()
+    {
+        // Go to the center of the player
+        transform.position = playerTransform.position;
+        transform.rotation = playerTransform.rotation;
+
+        // Move out by the offset
+        transform.Translate(-offset);
+
+        // Reset rotation
+        transform.LookAt(playerTransform);
+    }
+}
