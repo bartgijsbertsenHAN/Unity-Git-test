d0827d0d1b1de602903783f3c9a747d0c600fbc3
Bart Gijsbertsen <GLJ.Gijsbertsen@student.han.nl>
2020-10-02 14:37:10 +0200

    Player can move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float movementSpeed;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = movementSpeed * new Vector3(verticalMovement, 0, horizontalMovement).normalized;
+
+        transform.Translate(movement, Space.Self);
+    }
+}
